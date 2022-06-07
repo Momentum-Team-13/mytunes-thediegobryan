d9d5e27a67ebf95aed9c0127e568413146ac52d1
Diego Bryan <thediegobryan@gmail.com>
2022-06-07 09:28:53 -0400

    added individual cards for each possible search result

--- main.js
+++ main.js
@@ -1,21 +1,58 @@
 let url = "https://itunes.apple.com/search?term=shakira";
 
-let songSelection = document.querySelector('.song');
-let songPreview = document.querySelector('.songPreview')
+let songSelection = document.querySelector('#song-preview');
+let songPreview = document.querySelector('#song-preview-audio')
+let searchResult = document.querySelector('#search-result')
 
-function previewSong(searchResults){
-    let songName = searchResults.results[45].trackName;
-    let artistName = searchResults.results[45].artistName;
-    let source = searchResults.results[45].previewUrl;
+// TO DO LIST
+// ========================================
+// -Need to add click event listener to each search result element like I did for each calculator GamepadButton. This function will tell the page to change the song preview to the clicked song.
+//
+//- for the search bar, my guess is that i'll us the searchbox.innerText and add that to the fetch url when button is clicked. I think an event listener will make the button work?
 
-    let previewSongSource = document.createElement("source")
-    previewSongSource.src = source
-    songPreview.appendChild(previewSongSource)
 
-    let newSong = document.createElement("h1")
-    newSong.innerText = `${songName} By ${artistName}`
-    songSelection.appendChild(newSong)
+function previewSong(searchData){
+    let songName = searchData.results[45].trackName;
+    let artistName = searchData.results[45].artistName;
+    let source = searchData.results[45].previewUrl;
 
+    let previewSongSource = document.createElement("source");
+    previewSongSource.src = source;
+    songPreview.appendChild(previewSongSource);
+
+    let newSong = document.createElement("h1");
+    newSong.innerText = `${songName} By ${artistName}`;
+    songSelection.appendChild(newSong);
+}
+
+function searchgrid(searchData){
+    for(result of searchData){
+        //Container Box
+        let resultBox = document.createElement("div")
+        resultBox.classList.add("card")
+        resultBox.style.width = "12rem";
+        resultBox.id = "result-box";
+        searchResult.appendChild(resultBox)
+        //Img Thumb
+        let resultImg = document.createElement("img");
+        resultImg.src = result.artworkUrl100;
+        resultImg.classList.add("card-img-top");
+        resultBox.appendChild(resultImg);
+        //cardBody
+        let cardBody = document.createElement("div");
+        cardBody.classList.add("card-body");
+        resultBox.appendChild(cardBody);
+        //Song Title
+        let songTitle = document.createElement("h5");
+        songTitle.classList.add("card-title");
+        songTitle.innerText = result.trackName;
+        cardBody.appendChild(songTitle);
+        //Artist Name
+        let artist = document.createElement("p");
+        artist.classList.add("card-text");
+        artist.innerText = result.artistName;
+        cardBody.appendChild(artist);
+    }
 }
 
 
@@ -27,6 +64,7 @@ fetch(url, {
     return response.json()
     })
     .then(function (data) {
-    console.log('You have been successfully subscribed', data.results[45])
+    console.log('You have been successfully subscribed', data.results)
     previewSong(data)
+    searchgrid(data.results)
     })
